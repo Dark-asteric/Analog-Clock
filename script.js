8660793edadd66f615f93e2ab89bcc9a1d0f40f0
Muhammad MI Shafi <111156790+Dark-asteric@users.noreply.github.com>
2023-03-15 12:17:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let hr = document.querySelector('#hr');
+let mn = document.querySelector('#mn');
+let sc = document.querySelector('#sc');
+
+setInterval(() =>{
+	let day = new Date();
+	let hh = day.getHours() * 30;
+	let mm = day.getMinutes() * 6;
+	let ss = day.getSeconds() * 6;
+		
+	hr.style.tranform = `rotateZ(${hh+(mm/12)}deg)`;
+	mn.style.transform = `rotateZ(${mm}deg)`;
+	sc.style.transform = `rotateZ(${ss}deg)`;
+	
+	// Digital Clock
+	
+	let hours = document.getElementById('hours');
+	let minutes = document.getElementById('minutes');
+	let seconds = document.getElementById('seconds');
+	let ampm = document.getElementById('ampm');
+	
+	let h = new Date().getHours();
+	let m = new Date().getMinutes();
+	let s = new Date().getSeconds();
+	
+	hours.outerHTML = h;
+	minutes.outerHTML = m;
+	seconds.outerHTML = s;
+})
+
+
